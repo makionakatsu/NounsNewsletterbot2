cac66e9632544082676a12aaa17d341d876bc0a0
maki_ <ny.2151102@gmail.com>
2023-07-22 13:22:12 +0900

    main入れ替え

--- main.py
+++ main.py
@@ -1,181 +1,127 @@
-import email
-from email.header import decode_header
-import imaplib
-from bs4 import BeautifulSoup
-import requests
-import openai
 import os
-import time
+import pandas as pd
+import openai
+import datetime
+import nextcord as discord
+from nextcord.ext import commands
+import pytz
+import csv
 
-# メールサーバーに接続する関数
-def connect_mail_server(email, password):
-    mail = imaplib.IMAP4_SSL("imap.gmail.com")
-    mail.login(email, password)
-    mail.select("inbox")
-    return mail
+# GitHub Secretsから各種キーを読み込む
+openai.api_key = os.getenv('OPENAI_API_KEY')
+guild_id = os.getenv('GUILD_ID')
+summary_channel_id = os.getenv('SUMMARY_CHANNEL_NAME')
+discord_token = os.getenv('DISCORD_TOKEN')
 
-# 未読のメールIDを取得する関数
-def get_unread_mail_ids(mail):
-    _, data = mail.search(None, "UNSEEN")
-    mail_ids = data[0].split()
-    return mail_ids
+# CSVファイルに出力する項目
+fieldnames = ['Timestamp', 'Channel', 'Author', 'Content', 'Message URL', 'Emoji Count']
 
-# BeautifulSoupオブジェクトから本文とURLを抽出する関数
-def get_contents(soup):
-    contents = []
-    for tag in soup.find_all(["h1", "h2", "h3", "p", "a", "li", "ol"]):
-        if tag.name == "a":
-            content = (tag.name, tag.get("href"))
-        else:
-            content = (tag.name, tag.get_text(strip=True))
-        contents.append(content)
-    return contents
+# CSVファイルを作成する関数を定義
+async def write_chat_to_csv(bot):
+    # 現在の日付を取得し、それを文字列形式に変換
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
 
-# メールを処理し、本文とURLを取得する関数
-def process_mail(mail_id, mail):
-    _, msg_data = mail.fetch(mail_id, "(RFC822)")
-    raw_email = msg_data[0][1]
-    raw_mail = email.message_from_bytes(raw_email)
+    # 現在の日付を含むCSVファイル名を設定
+    csv_file = f"discord_log_{current_date}.csv"
 
-    subject = decode_header(raw_mail["Subject"])[0][0]
-    if isinstance(subject, bytes):
-        decoded_subject = subject.decode()
-    else:
-        decoded_subject = subject
+    with open(csv_file, 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        guild = bot.get_guild(int(guild_id))
+        for channel in guild.text_channels:
+            # 各メッセージに対して処理
+            async for message in channel.history(limit=None):
+                writer.writerow({
+                    'Timestamp': message.created_at.astimezone(pytz.timezone('Asia/Tokyo')).strftime('%Y-%m-%d %H:%M:%S'),
+                    'Channel': channel.name,
+                    'Author': message.author.name,
+                    'Content': message.content,
+                    'Message URL': message.jump_url,
+                    'Emoji Count': len(message.reactions)
+                })
 
-    received_date = raw_mail["Date"]
-
-    if raw_mail.is_multipart():
-        for part in raw_mail.walk():
-            if part.get_content_type() == "text/html":
-                html_content = part.get_payload(decode=True).decode()
-                soup = BeautifulSoup(html_content, "html.parser")
-                contents = get_contents(soup)
-
-    return received_date, decoded_subject, contents
-
-# メッセージをフォーマットに合わせて整形する関数
-def format_messages(decoded_subject, contents):
-    formatted_messages = []
-    message = f"**{decoded_subject}**\n\n⌐◨-◨ ⌐◨-◨ ⌐◨-◨ ⌐◨-◨ ⌐◨-◨ ⌐◨-◨"
-    for content_type, content in contents:
-        if content_type == "h1" or content_type == "h2":
-            message += f"\n\n{content}"
-        elif content_type == "h3":
-            message += f"\n\n📘 **{content}**"
-        elif content_type == "p":
-            summary = summarize_text(content)
-            message += f"\n・{summary}"
-        elif content_type == "a":
-            url_line = f"\n🔗{content}"
-            if len(message) + len(url_line) > 2000:
-                # URLを追加すると2000文字を超えるため、新しいメッセージを開始
-                formatted_messages.append(message)
-                message = "⌐◨-◨ ⌐◨-◨ ⌐◨-◨ ⌐◨-◨ ⌐◨-◨ ⌐◨-◨" + url_line
-            else:
-                message += url_line
-        elif content_type == "li":
-            message += f"\n- {content}"
-        elif content_type == "ol":
-            message += f"\n1. {content}"
-
-    formatted_messages.append(message)
-    return formatted_messages
-
-# テキストを要約する関数
-def summarize_text(text):
-    print(f"Summarizing the following text: {text}")  
+# GPT-3.5-turboを使ってテキストを要約する関数を定義
+def summarize_with_gpt(text):
+    # GPT-3.5-turboを使ってテキストを要約
     response_summary = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-16k",
         messages=[
-            {"role": "system", "content": "You are an assistant who summarizes news articles in Japanese into about 200 characters. You can generate interesting sentences."},
-            {"role": "user", "content": f"Here's a news article: {text}. Can you summarize it for me?"},
+            {"role": "system", "content": "You are an assistant who summarizes discord chat in Japanese."},
+            {"role": "user", "content": f"Here's a discord chat: {text}. Can you summarize it for me in japanese?"},
         ],
         max_tokens=300
     )
+
+    # 要約結果を取得
     summary = response_summary['choices'][0]['message']['content']
+
     return summary
 
-# Discordにメッセージを送信する関数
-def send_discord_message(webhook_url, content, max_retries=3, retry_delay=5):
-    chunks = []
-    current_chunk = ""
-    for sentence in content.split("."):
-        if len(current_chunk) + len(sentence) + 1 > 1999:
-            chunks.append(current_chunk)
-            current_chunk = sentence
-        else:
-            current_chunk += "." if current_chunk else ""
-            current_chunk += sentence
-    if current_chunk:
-        chunks.append(current_chunk)
+# Discordチャットの要約を作成する関数を定義
+def summarize_discord_chat(csv_file):
+    # CSVファイルを読み込み
+    df = pd.read_csv(csv_file)
 
-    for chunk in chunks:
-        if isinstance(chunk, bytes):
-            chunk = chunk.decode('utf-8')
+    # データをチャンネルと絵文字数でソート
+    df_sorted = df.sort_values(['Channel', 'Emoji Count'], ascending=[True, False])
 
-        data = {"content": chunk}
-        retries = 0
+    # 各チャンネルの上位5件のコメントを取得
+    top_comments = df_sorted.groupby('Channel').head(5)
+    top_comments = top_comments.sort_values(['Channel', 'Emoji Count'], ascending=[True, False])
 
-        while retries <= max_retries:
-            response = requests.post(webhook_url, json=data)
-            if response.status_code == 204:
-                break
-            elif response.status_code == 429:
-                delay = int(response.headers['Retry-After'])
-                time.sleep(delay)
-                retries += 1
-            else:
-                retries += 1
-                if retries < max_retries:
-                    time.sleep(retry_delay)
+    channel_summary_dict = {}
+    for channel, group in top_comments.groupby('Channel'):
+        # チャンネル内の上位コメントをすべて連結
+        text = ' '.join(group['Content'].values)
 
-# メイン関数
-def main():
-    email = os.environ["EMAIL"]
-    password = os.environ["PASSWORD"]
-    webhook_url = os.environ["WEBHOOK_URL"]
-    openai.api_key = os.environ["OPENAI_KEY"]
+        # GPT-3.5-turboを使ってチャットを要約
+        summary = summarize_with_gpt(text)
 
-    # メールサーバーに接続
-    mail = connect_mail_server(email, password)
-    # 未読のメールIDを取得
-    mail_ids = get_unread_mail_ids(mail)
+        # 要約結果と上位コメントを保存
+        channel_summary_dict[channel] = {'summary': summary, 'top_comments': group[['Content', 'Message URL']].values.tolist()}
 
-    if len(mail_ids) == 0:
-        print("No unread mails found. Skipping Discord message sending.")
-    else:
-        for mail_id in mail_ids:
-            # メールを処理し、記事と件名を取得
-            received_date, decoded_subject, h1, h2, li, contents = process_mail(mail_id, mail)
+    return channel_summary_dict
 
-            formatted_messages = []
+# Discordチャンネルにメッセージを送信する関数を定義
+async def send_message_to_discord(bot, channel_id, message):
+    # チャンネルIDからチャンネルオブジェクトを取得
+    target_channel = bot.get_channel(int(channel_id))
 
-            # メール受信日と件名をメッセージに追加
-            formatted_messages.append(f"**{received_date}**\n**{decoded_subject}**\n\n⌐◨-◨ ⌐◨-◨ ⌐◨-◨ ⌐◨-◨ ⌐◨-◨ ⌐◨-◨")
+    # メッセージを送信
+    await target_channel.send(message)
 
-            # h1, h2, liが存在する場合はメッセージに追加
-            if h1:
-                formatted_messages.append(h1)
-            if h2:
-                formatted_messages.append(h2)
-            if li:
-                formatted_messages.extend(li)
-
-            for subtitle, text, urls in contents:
-                # 本文を要約
-                summary = summarize_text(text)
-
-                # URLをフォーマットに合わせて整形
-                formatted_urls = "\n".join([f"🔗URL: {url}" for url in urls])
-                # メッセージをフォーマットに合わせて整形
-                message = f"\n\n⌐◨-◨ ⌐◨-◨ ⌐◨-◨ ⌐◨-◨ ⌐◨-◨ ⌐◨-◨\n\n📘 **{subtitle}**\n・{summary}\n{formatted_urls}\n\n"
-                formatted_messages.append(message)
-
-            # 全てのメッセージを結合
-            formatted_output = "\n".join(formatted_messages)
-            # Discordにメッセージを送信
-            send_discord_message(webhook_url, formatted_output)
-
+# メインの処理
 if __name__ == "__main__":
-    main()
+    # Botの準備
+    bot = commands.Bot(command_prefix='!')
+
+    @bot.event
+    async def on_ready():
+        print('We have logged in as {0.user}'.format(bot))
+
+        # CSVファイルを作成
+        await write_chat_to_csv(bot)
+
+        # 現在の日付を取得し、それを文字列形式に変換
+        current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+        # 現在の日付を含むCSVファイル名を設定
+        csv_file = f"discord_log_{current_date}.csv"
+
+        # CSVファイルを読み込んでDiscordチャットを要約
+        summaries = summarize_discord_chat(csv_file)
+
+        # 各チャンネルの要約結果をDiscordチャンネルに送信
+        for channel, data in summaries.items():
+            message = f"======================\n"
+            message += f"Channel: {channel}\n"
+            message += data['summary'] + "\n"
+            message += "【話題ピックアップ】\n"
+            for comment, url in data['top_comments']:
+                message += f"・{comment} ({url})\n"
+            message += f"======================\n"
+
+            await send_message_to_discord(bot, summary_channel_id, message)
+
+    # Botを起動
+    bot.run(discord_token)
